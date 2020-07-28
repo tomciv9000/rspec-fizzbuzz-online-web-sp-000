138dda9b088f5cce222835cf804d832d4afbe363
Tom White <eugene.white@gmail.com>
2020-07-28 05:27:58 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,12 +2,12 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0
-    "FizzBuzz"
+    return "FizzBuzz"
   elsif int % 3 == 0
-    "Fizz"
-  elsif int % 5 == 0 
-    "Buzz"
-  
+    return "Fizz"
+  elsif int % 5 == 0
+    return "Buzz"
+  else
+    return nil 
   end
-  
 end
